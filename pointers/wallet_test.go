dfef1fa7a12c3fc8fe085df86f99ebe381474560
charans29 <smodugula@csuchico.edu>
2024-05-18 23:48:22 -0700

    fixing the code

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -13,22 +13,26 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
-		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
-
+	
+		assertError(t, err)
 		assertBalance(t, wallet, startingBalance)
-
-	i	if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
 	})
 }
 
